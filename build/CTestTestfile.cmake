6bf87dfede1e84c55e2eaab589e1015ce798c63d
shsdbd <alsdyd114@gmail.com>
2026-03-16 18:05:44 +0900

    fix: completely remove local build artifacts and cache

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/alsdy/Documents/10th/RVC_Project
-# Build directory: C:/Users/alsdy/Documents/10th/RVC_Project/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test([=[MyTest]=] "C:/Users/alsdy/Documents/10th/RVC_Project/build/run_tests.exe")
-set_tests_properties([=[MyTest]=] PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/alsdy/Documents/10th/RVC_Project/CMakeLists.txt;65;add_test;C:/Users/alsdy/Documents/10th/RVC_Project/CMakeLists.txt;0;")
