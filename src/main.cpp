453b420d0ee2ef8a4cb040c0f6dbfe50a46831db
shsdbd <alsdyd114@gmail.com>
2026-03-16 18:12:36 +0900

    fix-main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,14 @@
 ﻿// src/main.cpp
 #include <iostream>
 int main() {
-    // 1. 명백한 메모리 누수 (Malloc/New 활용)
-    int* leak = new int[1000];
+    // // 1. 명백한 메모리 누수 (Malloc/New 활용)
+    // int* leak = new int[1000];
 
-    // 2. Clang-Tidy가 싫어하는 'if문 안에서 대입' (bugprone-assignment-in-if-condition)
-    int a = 0;
-    if (a = 10) {
-        std::cout << "This should trigger an error!" << std::endl;
-    }
+    // // 2. Clang-Tidy가 싫어하는 'if문 안에서 대입' (bugprone-assignment-in-if-condition)
+    // int a = 0;
+    // if (a = 10) {
+    //     std::cout << "This should trigger an error!" << std::endl;
+    // }
 
     return 0;  // leak을 해제하지 않고 종료
 }
